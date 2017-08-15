dedbd8e482559ec82d5b062bc6e8edf0bd88a683
Dunstan Vavasour <DVavasou@gb.computacenter.co.uk>
2017-08-15 16:42:52 +0100

    Not sure what I'm doing here

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ variable "ami-image" {
 data "aws_availability_zones" "all" {}
 
 provider "aws" {
-  region = "eu-west-2"
+    region = "eu-west-2"
 }
 
 resource "aws_launch_configuration" "example" {
@@ -32,42 +32,42 @@ resource "aws_launch_configuration" "example" {
 }
 
 resource "aws_autoscaling_group" "example" {
-  launch_configuration = "${aws_launch_configuration.example.id}"
-  availability_zones = ["${data.aws_availability_zones.all.names}"]
+    launch_configuration = "${aws_launch_configuration.example.id}"
+    availability_zones = ["${data.aws_availability_zones.all.names}"]
 
-  load_balancers = ["${aws_elb.example.name}"]
-  health_check_type = "ELB"
-  
-  min_size = 2
-  max_size = 20
+    load_balancers = ["${aws_elb.example.name}"]
+    health_check_type = "ELB"
+    
+    min_size = 2
+    max_size = 20
 
-  tag {
-    key = "Name"
-    value = "terraform-asg-example"
-    propagate_at_launch = true
-  }
+    tag {
+	key = "Name"
+	value = "terraform-asg-example"
+	propagate_at_launch = true
+    }
 }
 
 resource "aws_elb" "example" {
-  name = "terraform-asg-example"
-  availability_zones = ["${data.aws_availability_zones.all.names}"]
-  security_groups = ["${aws_security_group.elb.id}"]
+    name = "terraform-asg-example"
+    availability_zones = ["${data.aws_availability_zones.all.names}"]
+    security_groups = ["${aws_security_group.elb.id}"]
 
-  listener {
-    lb_port = 80
-    lb_protocol = "http"
-    instance_port = "${var.server_port}"
-    instance_protocol = "http"
-  }
+    listener {
+	lb_port = 80
+	lb_protocol = "http"
+	instance_port = "${var.server_port}"
+	instance_protocol = "http"
+    }
 
-  health_check {
-    healthy_threshold = 2
-    unhealthy_threshold = 2
-    timeout = 3
-    interval = 10
-    target = "HTTP:${var.server_port}/"
-  }
-  
+    health_check {
+	healthy_threshold = 2
+	unhealthy_threshold = 2
+	timeout = 3
+	interval = 10
+	target = "HTTP:${var.server_port}/"
+    }
+    
 }
 
 resource "aws_security_group" "instance" {
@@ -86,22 +86,22 @@ resource "aws_security_group" "instance" {
 }
 
 resource "aws_security_group" "elb" {
-  name = "terraform-example-elb"
+    name = "terraform-example-elb"
 
 
-  ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+    ingress {
+	from_port = 80
+	to_port = 80
+	protocol = "tcp"
+	cidr_blocks = ["0.0.0.0/0"]
+    }
 
-  egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+    egress {
+	from_port = 0
+	to_port = 0
+	protocol = "-1"
+	cidr_blocks = ["0.0.0.0/0"]
+    }
 }
 
 output "public_ip" {
